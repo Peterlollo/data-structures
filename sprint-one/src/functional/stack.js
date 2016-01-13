54907aa8d867186f1b935c8d0fcb7d0dbc828912
peterlollo <peteralollo@gmail.com>
2016-01-13 14:54:42 -0800

    Implement a stack constructor function

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,24 @@
 var Stack = function() {
   var someInstance = {};
-
+  var stackSize = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+  stackSize++;
+  someInstance[stackSize] = value;
   };
 
   someInstance.pop = function() {
+  var result = someInstance[stackSize];
+  delete someInstance[stackSize];
+  stackSize--;
+  return result;
   };
 
   someInstance.size = function() {
+  return Math.max(0,stackSize);
   };
 
   return someInstance;
