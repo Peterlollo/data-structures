a653891ae32ac9bd3c8457993b9830c993b7b3f9
peterlollo <peteralollo@gmail.com>
2016-01-13 15:27:27 -0800

    Implement the queue constructor function

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,6 @@
 var Queue = function() {
   var someInstance = {};
+  var queueSize = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,13 +8,27 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    someInstance[queueSize] = value;
+    queueSize++;
   };
 
   someInstance.dequeue = function() {
+    var result = someInstance[0];
+    delete someInstance[0];
+    queueSize--;
+    for(var key in someInstance) {
+      someInstance[key -1] = someInstance[key];
+    }
+    return result;
   };
 
   someInstance.size = function() {
+    if(queueSize<0) {
+      queueSize = 0;
+    }
+    return queueSize;
   };
+
 
   return someInstance;
 };
