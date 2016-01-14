46564ae0b33b541da26c59436345f133ed69619f
peterlollo <peteralollo@gmail.com>
2016-01-13 16:36:09 -0800

    Implement stack constructor function using shared methods

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
+  var newStack = {};
+  // var stackSizeValue = 0;
+  newStack.stackSize = 0;
+  // newStack.storage = {};
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+_.extend(newStack, stackMethods);
+
+return newStack;
 };
 
-var stackMethods = {};
+  stackMethods = {};
+  stackMethods.size = function() {
+    return Math.max(0,this.stackSize);
+  };
 
+  stackMethods.push = function(value) {
+    // this.storage[this.stackSize] = value;
+    this.stackSize++;
+    var diffKey = this.stackSize;
+    this[diffKey] = value;
+  };
+
+  stackMethods.pop = function() {
+    // var result = this.storage[this.stackSize];
+    // delete this.storage[this.stackSize];
+    var diffKey = this.stackSize;
+    var result = this[diffKey];
+    delete this[diffKey];
+    this.stackSize--;
+    return result;
+    // return result;
+  };
 
