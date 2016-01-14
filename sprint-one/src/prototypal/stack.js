1ea792aafa872ec1ebc1423306f14398a060731c
peterlollo <peteralollo@gmail.com>
2016-01-13 20:52:43 -0800

    Implement the stack constructor function with the prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.stackSize = 0;
+
+
+  return newStack;
 };
 
 var stackMethods = {};
+
+stackMethods.size = function(){
+  return Math.max(0,this.stackSize);
+};
+
+stackMethods.push = function(value){
+  this.stackSize++;
+  this[this.stackSize] = value;
+};
+
+stackMethods.pop = function(){
+  var result = this[this.stackSize];
+  delete this[this.stackSize];
+  this.stackSize--;
+  return result;
+};
 
 
