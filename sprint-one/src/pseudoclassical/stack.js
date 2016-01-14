29ae4fc02c41b593f1c33299e8d85701284f9d9d
peterlollo <peteralollo@gmail.com>
2016-01-13 21:25:35 -0800

    Implement the stack function with the pseudoclassical pattern

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
 var Stack = function() {
+
+  this.stackSize = 0;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
+Stack.prototype.size = function(){
+  return Math.max(0, this.stackSize);
+};
+
+Stack.prototype.push = function(value){
+  this.stackSize++;
+  this[this.stackSize] = value;
+};
+
+Stack.prototype.pop = function(){
+  var result = this[this.stackSize];
+  delete this[this.stackSize];
+  this.stackSize--;
+  return result;
+};
 
