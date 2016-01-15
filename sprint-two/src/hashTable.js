830c2fea8eff2f643e2369e329de319f34f09401
peterlollo <peteralollo@gmail.com>
2016-01-15 11:41:06 -0800

    Implement hash table constructor function to bare minimum

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,29 +8,47 @@ var HashTable = function() {
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   // console.log("index", index);
-  this._storage.set(index, v);
-  //if the storage[index] is not undefined
-    //retain some reference in Intermediary Storage Object between desired index and key and actual index
-    //but reassign key to a new index and set it there
+  if(this._storage.get(index) === undefined){
+    this._storage.set(index, []);
+    }
+    var bucket = this._storage.get(index);
+    var alreadyInBucket = false;
+    for(var i = 0; i < bucket.length; i++) {
+      if(bucket[i][0] === k) {
+        bucket[i][1] = v;
+        alreadyInBucket = true;
+      }
+    }
+    if(!alreadyInBucket){
+     bucket.push([k,v]);
+    }
+
+  
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
-  //before you return the value located at a given index
-    //check Intermediary Storage Object to see if some other value also wanted that index originally
-      //if so, do some function that associates the key with the value you want and return the value;
+    var bucket = this._storage.get(index);
+      for(var i = 0; i < bucket.length; i ++) {
+        if(bucket[i][0] === k) {
+          return bucket[i][1];
+        }
+      }
+
+
 
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-   this._storage.set(index, undefined);
-   // for(var key in this._storage) {
-   //  if (this._storage[key] === undefined) {
-   //    delete key;
-   //  }
-   // }
+  var bucket = this._storage.get(index);
+  for(var i = 0; i < bucket.length; i ++) {
+    if(bucket[i][0] === k) {
+      bucket.splice(i, 1);
+    }
+  }
+
+
 };
 
 
