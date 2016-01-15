dc266a9c7ccc2ce68b6046c3b7241934c6d067f6
peterlollo <peteralollo@gmail.com>
2016-01-14 16:25:58 -0800

    Implement graph constructor function with pseudoclassical pattern for storing and removing nodes

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,21 +3,35 @@
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
+
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.node = node;
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  var result = false;
+  for(var key in this) {
+    if(this[key] === node) {
+      result = true;
+    }
+  }
+  return result;
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  for(var key in this) {
+    if(this[key] === node) {
+      delete this[key];
+    } 
+  }
 };
 
 // ------------------------
