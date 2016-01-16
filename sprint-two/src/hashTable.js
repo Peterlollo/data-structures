44ddd289316ee190ee6b8113f3f8edf06307075e
peterlollo <peteralollo@gmail.com>
2016-01-15 22:38:53 -0800

    Attempt at doubling hashtable when required

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,11 +3,26 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  //Reassign tempStorage to the old limitedArray(makes it easier to reassign)
+  this._tempStorage = this._storage;
   this._tupleCount = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
   this._tupleCount++;
+  if((this._tupleCount/this._limit) > 0.75) {
+    //Double the limit
+    this._limit = this._limit*2;
+    //Overwrite the old storage by rehashing now that there is a new limit!
+      this._storage.each(function(bucket, bucketIndex){
+        for(var i = 0; i < bucket.length; i++) {
+          //Yet to figure out this bit 
+          this._tempStorage.insert();
+
+        }
+      });
+    }
+
   var index = getIndexBelowMaxForKey(k, this._limit);
   // console.log("index", index);
   if(this._storage.get(index) === undefined){
@@ -40,6 +55,13 @@ HashTable.prototype.retrieve = function(k) {
 
 
 };
+
+//If the tuple count divided by the limit is greater than 0.75:
+      //Initiate a new limitedArray with a higher limit
+        //Re hash every item from the old limitedArray into the new limitedArray!
+          //Reset this._storage to that var once you're done!
+          //Delete old hash table using the old limit to identify it(store old limit in a var!!?) 
+//
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
