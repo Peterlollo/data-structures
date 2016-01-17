9bdae247a820d9e3d58aeff313ea99071d18fff9
peterlollo <peteralollo@gmail.com>
2016-01-16 16:03:38 -0800

    Perform time complexity analysis

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -36,3 +36,5 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+//Constant time operations: addChild()
+//Linear time operations: contains()
